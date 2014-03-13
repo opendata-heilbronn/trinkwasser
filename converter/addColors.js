81ea2da3b7e299703e1665dbdb9c33e20d9cd80f
Felix Ebert <felix.ebert@if-core.de>
2014-03-13 21:42:34 +0100

    converter

---
+++ converter/addColors.js
@@ -0,0 +1,39 @@
+var fs = require('fs');
+var argv = require('optimist').usage('Add colors to json file with street points\nUsage: $0 [jsonfile]').demand(1).argv;
+
+var colors = {
+	'25': '#08306b',
+	'14-18': '#2171b5',
+	'14': '#4292c6',
+	'10': '#9ecae1',
+	'9': '#deebf7'
+};
+
+var geojson = fs.readFile(argv._[0], {
+	encoding: 'utf-8'
+}, function(err, data) {
+	var geojson = JSON.parse(data);
+
+	var values = {};
+	geojson.features.forEach(function(feature) {
+		var haerte = feature.properties.haerte;
+		if (haerte) {
+			haerte = haerte.trim();
+			if (haerte != '10') {
+				haerte = haerte.substr(0, haerte.length - 1);
+			}
+			if (haerte == '14°') {
+				haerte = haerte.substr(0, haerte.length - 1);
+			}
+			if (haerte && haerte != '-1' && haerte != '14-1') {
+				if (!values[haerte]) {
+					feature.properties['marker-symbol'] = 'circle-stroked';
+					feature.properties['marker-color'] = colors[haerte];
+				}
+			}
+		}
+	});
+
+	var geojsonString = JSON.stringify(geojson, null, '\t');
+	fs.writeFile('result-with-colors.geojson', geojsonString);
+});
