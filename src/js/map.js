280dfca59f3c58d45dc7e0ec845ff984884216fe
Felix Ebert <felix.ebert@if-core.de>
2014-03-21 07:41:53 +0100

    async map

--- src/js/map.js
+++ src/js/map.js
@@ -1,6 +1,8 @@
 (function(tw, d3) {
 	'use strict';
 
+	var areaData = null, zoneData = null;
+
 	var Map = function(svg) {
 		var width = 1170;
 		var height = 1170;
@@ -10,7 +12,7 @@
 		var projection = d3.geo.mercator().scale(scale).center(center).translate(offset);
 		var path = d3.geo.path().projection(projection);
 
-		var bounds = path.bounds(tw.data.geoLayer);
+		var bounds = path.bounds(areaData);
 		var hscale = scale * width / (bounds[1][0] - bounds[0][0]);
 		var vscale = scale * height / (bounds[1][1] - bounds[0][1]);
 		var scale = (hscale < vscale) ? hscale : vscale;
@@ -21,8 +23,8 @@
 
 		var z = d3.scale.linear().domain([9, 16]).range(colorbrewer.RdBu[9]);
 
-		svg.select('.areas').selectAll("path").data(tw.data.geoLayer.features).enter().append("path").attr("d", path);
-		svg.select('.zones').selectAll("path").data(tw.data.zonesGeo.features).enter().append("path").attr("d", path).attr('stroke', function(d) {
+		svg.select('.areas').selectAll("path").data(areaData.features).enter().append("path").attr("d", path);
+		svg.select('.zones').selectAll("path").data(zoneData.features).enter().append("path").attr("d", path).attr('stroke', function(d) {
 			return z(tw.utils.getMeanValue(d.properties.haertegrad));
 		});
 	};
@@ -33,7 +35,13 @@
 	};
 
 	var init = function() {
-		mapInstance = new Map(d3.select('.map-img'));
+		$.getJSON('data/gemeinden_simplify20.geojson', function(data) {
+			areaData = data;
+			$.getJSON('data/hn-streets.geojson', function(hnData) {
+				zoneData = hnData;
+				mapInstance = new Map(d3.select('.map-img'));
+			});
+		});
 		return this;
 	};
 
