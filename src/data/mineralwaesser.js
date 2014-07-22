2a3f0f53f32d5a594381c607a7eacbc2b562084e
Ralph Haussmann <ralph@scanmyfood.de>
2014-07-22 18:57:58 +0200

    Use an option control to select a mineral water for comparison
    - Remove buttons and use an option control instead
    - Don't let user compare his tap water with his tap water. That feature was very confusing.

--- src/data/mineralwaesser.js
+++ src/data/mineralwaesser.js
@@ -1,5 +1,5 @@
 tw.data.referenceWaters = {
-	"wueteria": {
+	"Wüteria": {
 		"natrium": 10.7,
 		"kalium": 2.6,
 		"calcium": 118,
@@ -9,7 +9,7 @@ tw.data.referenceWaters = {
 		"sulfat": 68,
 		"sources": ["http://www.wueteria.de/unser_wasser/index.php"]
 	},
-	"teusser": {
+	"Teusser": {
 		"natrium": 53,
 		"kalium": 8,
 		"calcium": 565,
@@ -19,7 +19,7 @@ tw.data.referenceWaters = {
 		"sulfat": 1450,
 		"sources": ["http://www.teusser.de/index.php?id=225"]
 	},
-	"volvic": {
+	"Volvic": {
 		"natrium": 11.6,
 		"kalium": 6.2,
 		"calcium": 11.5,
@@ -29,7 +29,7 @@ tw.data.referenceWaters = {
 		"sulfat": 8.1,
 		"sources": ["http://books.google.de/books?id=lzEoGWyqMBwC&pg=PA198&lpg=PA198&dq=volvic+nitratgehalt&source=bl&ots=pJEE0i9HwK&sig=OI_PeSr_QGBCfpeVCu70N4ohy8g&hl=de&ei=4-yvSuHrJ8jK_gbV0p3ZDA&sa=X&oi=book_result&ct=result&resnum=6#v=onepage&q&f=false"]
 	},
-	"vittel": {
+	"Vittel": {
 		"natrium": 7.7,
 		"kalium": 5.2,
 		"calcium": 94,
