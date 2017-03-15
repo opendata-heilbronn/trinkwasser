b8fdf713fe4c0a9102391a4fe2f37e6347446355
Duncan Healy <duncan.healy@gmail.com>
2017-03-15 15:56:37 +0000

    standard format

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,64 +1,64 @@
-module.exports = function(grunt) {
-	'use strict';
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		uglify: {
-			options: {
-				banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-			}
-		},
-		jshint: {
-			files: ['gruntfile.js', 'src/js/*.js'],
-			options: {
-				globals: {
-					jQuery: true,
-					console: true,
-					window: true,
-					dw: true,
-					L: true
-				},
-				laxbreak: true
-			}
-		},
-		rev: {
-			options: {
-				encoding: 'utf8',
-				algorithm: 'md5',
-				length: 8
-			},
-			assets: {
-				files: [{
-					src: ['dist/{css,js}/*.{js,css}']
-				}]
-			}
-		},
-		watch: {
-			files: ['<%= jshint.files %>'],
-			tasks: ['jshint']
-		},
-		useminPrepare: {
-			html: ['dist/**/index.html'],
-			options: {
-				dest: 'dist/'
-			}
-		},
-		usemin: {
-			html: ['dist/**/*.html'],
-			css: ['dist/**/*.css'],
-			options: {
-				dirs: ['dist/']
-			}
-		},
-		copy: {
-			dist: {
-				files: [{
-					expand: true,
-					dot: true,
-					cwd: 'src/',
-					dest: 'dist/',
-					src: ['*.{ico,txt}', 'img/{,*/}*.{jpg,png,svg,gif}', 'data/*.geojson', 'fonts/*', 'css/*', 'js/*', 'lib/*', 'data/*.js']
-				}]
-			},
+module.exports = function (grunt) {
+  'use strict'
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      }
+    },
+    jshint: {
+      files: ['gruntfile.js', 'src/js/*.js'],
+      options: {
+        globals: {
+          jQuery: true,
+          console: true,
+          window: true,
+          dw: true,
+          L: true
+        },
+        laxbreak: true
+      }
+    },
+    rev: {
+      options: {
+        encoding: 'utf8',
+        algorithm: 'md5',
+        length: 8
+      },
+      assets: {
+        files: [{
+          src: ['dist/{css,js}/*.{js,css}']
+        }]
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint']
+    },
+    useminPrepare: {
+      html: ['dist/**/index.html'],
+      options: {
+        dest: 'dist/'
+      }
+    },
+    usemin: {
+      html: ['dist/**/*.html'],
+      css: ['dist/**/*.css'],
+      options: {
+        dirs: ['dist/']
+      }
+    },
+    copy: {
+      dist: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: 'src/',
+          dest: 'dist/',
+          src: ['*.{ico,txt}', 'img/{,*/}*.{jpg,png,svg,gif}', 'data/*.geojson', 'fonts/*', 'css/*', 'js/*', 'lib/*', 'data/*.js']
+        }]
+      },
       redirect: {
         files: [{
           expand: true,
@@ -66,57 +66,57 @@ module.exports = function(grunt) {
           cwd: 'src/',
           dest: 'dist/',
           src: ['redirect.html'],
-          rename: function(dest, src) {
-              return dest + 'index.html';
-            }
+          rename: function (dest, src) {
+            return dest + 'index.html'
+          }
         }]
       }
-		},
-		clean: {
-			dist: {
-				files: [{
-					dot: true,
-					src: ['dist/{css,js,img}']
-				}]
-			}
-		},
-		imagemin: {
-			dist: {
-				files: [{
-					expand: true,
-					cwd: 'src/img',
-					src: '{,*/}*.{png,jpg,jpeg}',
-					dest: 'dist/img'
-				}]
-			}
-		},
-		jsonmin: {
-			dist: {
-				options: {
-					stripWhitespace: true,
-					stripComments: true
-				},
-				files: [{
-					expand: true,
-					cwd: 'src/data',
-					src: ['**/*.json'],
-					dest: 'dist/data',
-					ext: '.json'
-				}]
-			}
-		},
-		'gh-pages': {
-			options: {
-				base: 'dist'
-			},
-			src: ['**']
-		},
+    },
+    clean: {
+      dist: {
+        files: [{
+          dot: true,
+          src: ['dist/{css,js,img}']
+        }]
+      }
+    },
+    imagemin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'src/img',
+          src: '{,*/}*.{png,jpg,jpeg}',
+          dest: 'dist/img'
+        }]
+      }
+    },
+    jsonmin: {
+      dist: {
+        options: {
+          stripWhitespace: true,
+          stripComments: true
+        },
+        files: [{
+          expand: true,
+          cwd: 'src/data',
+          src: ['**/*.json'],
+          dest: 'dist/data',
+          ext: '.json'
+        }]
+      }
+    },
+    'gh-pages': {
+      options: {
+        base: 'dist'
+      },
+      src: ['**']
+    },
     abideCreate: {
       default: { // Target name.
         options: {
           template: 'lang/templates/LC_MESSAGES/messages.pot', // (default: 'locale/templates/LC_MESSAGES/messages.pot')
           languages: ['en', 'fr', 'es', 'nl', 'de'],
-          localeDir: 'lang/locale',
+          localeDir: 'lang/locale'
         }
       }
     },
@@ -125,7 +125,7 @@ module.exports = function(grunt) {
         src: 'src/js/**/*.js',
         dest: 'lang/templates/LC_MESSAGES/messages.pot',
         options: {
-          language: 'JavaScript',
+          language: 'JavaScript'
         }
       },
       html: {
@@ -133,9 +133,9 @@ module.exports = function(grunt) {
         dest: 'lang/templates/LC_MESSAGES/messages.pot',
         options: {
           keyword: '_',
-          language: 'swig',
+          language: 'swig'
         }
-      },
+      }
     },
     statici18n: {
       options: {
@@ -155,25 +155,25 @@ module.exports = function(grunt) {
         }]
       }
     }
-	});
+  })
 
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.loadNpmTasks('grunt-contrib-imagemin');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-jsonmin');
-	grunt.loadNpmTasks('grunt-rev');
-	grunt.loadNpmTasks('grunt-usemin');
-	grunt.loadNpmTasks('grunt-gh-pages');
-  grunt.loadNpmTasks('grunt-i18n-abide');
-  grunt.loadNpmTasks('hernanex3-grunt-static-i18n');
-	grunt.registerTask('dataupdate', ['jsonmin:dist']);
-	grunt.registerTask('build', ['clean:dist', 'i18n', 'imagemin', 'jsonmin', 'uglify', 'copy:dist', 'copy:redirect']);
-	grunt.registerTask('deploy', ['build', 'gh-pages']);
-	grunt.registerTask('default', ['build']);
-  grunt.registerTask('i18n', ['statici18n']);
-};
+  grunt.loadNpmTasks('grunt-contrib-uglify')
+  grunt.loadNpmTasks('grunt-contrib-cssmin')
+  grunt.loadNpmTasks('grunt-contrib-imagemin')
+  grunt.loadNpmTasks('grunt-contrib-jshint')
+  grunt.loadNpmTasks('grunt-contrib-watch')
+  grunt.loadNpmTasks('grunt-contrib-copy')
+  grunt.loadNpmTasks('grunt-contrib-concat')
+  grunt.loadNpmTasks('grunt-contrib-clean')
+  grunt.loadNpmTasks('grunt-jsonmin')
+  grunt.loadNpmTasks('grunt-rev')
+  grunt.loadNpmTasks('grunt-usemin')
+  grunt.loadNpmTasks('grunt-gh-pages')
+  grunt.loadNpmTasks('grunt-i18n-abide')
+  grunt.loadNpmTasks('hernanex3-grunt-static-i18n')
+  grunt.registerTask('dataupdate', ['jsonmin:dist'])
+  grunt.registerTask('build', ['clean:dist', 'i18n', 'imagemin', 'jsonmin', 'uglify', 'copy:dist', 'copy:redirect'])
+  grunt.registerTask('deploy', ['build', 'gh-pages'])
+  grunt.registerTask('default', ['build'])
+  grunt.registerTask('i18n', ['statici18n'])
+}
--- converter/create-lkhn-geojson.js
+++ converter/create-lkhn-geojson.js
@@ -1,54 +1,59 @@
-var fs = require('fs'), csv = require('csv'), async = require('async'), nominatim = require('nominatim');
+var fs = require('fs')
+var csv = require('csv')
+var async = require('async')
+var nominatim = require('nominatim')
 
-var trimAll = function(line) {
-	for ( var column = 0; column < line.length; column++) {
-		line[column] = line[column].trim();
-	}
-};
+var trimAll = function (line) {
+  for (var column = 0; column < line.length; column++) {
+    line[column] = line[column].trim()
+  }
+}
 
-var generateZoneId = function(line) {
-	var idParts = [];
-	var addIdPart = function(value) {
-		if (value && idParts.indexOf(value) < 0) {
-			idParts.push(value);
-		}
-	};
+var generateZoneId = function (line) {
+  var idParts = []
+  var addIdPart = function (value) {
+    if (value && idParts.indexOf(value) < 0) {
+      idParts.push(value)
+    }
+  }
 
-	addIdPart(line[0]);
-	addIdPart(line[1]);
-	if (line[2]) {
-		addIdPart(line[3]);
-	}
-	return idParts.join(' ');
-};
+  addIdPart(line[0])
+  addIdPart(line[1])
+  if (line[2]) {
+    addIdPart(line[3])
+  }
+  return idParts.join(' ')
+}
 
-csv().from('Wasserdaten Landkreis Heilbronn.csv').to.array(function(data) {
-	delete data[0];
+csv().from('Wasserdaten Landkreis Heilbronn.csv').to.array(function (data) {
+  delete data[0]
 
-	var locations = {};
-	data.forEach(function(line) {
-		trimAll(line);
+  var locations = {}
+  data.forEach(function (line) {
+    trimAll(line)
 
-		var locationName = line[0];
-		if (line[2] && line[2] != 'alle anderen Straßen') {
-			locationName += ', ' + line[2];
-		} else if (line[1] && line[1] != line[0] && line[1] != 'Kernstadt') {
-			locationName += ' ' + line[1];
-		}
+    var locationName = line[0]
+    if (line[2] && line[2] !== 'alle anderen Straßen') {
+      locationName += ', ' + line[2]
+    } else if (line[1] && line[1] !== line[0] && line[1] !== 'Kernstadt') {
+      locationName += ' ' + line[1]
+    }
 
-		locations[locationName] = generateZoneId(line);
-	});
+    locations[locationName] = generateZoneId(line)
+  })
 
-	var features = [];
-	async.eachLimit(Object.keys(locations), 1, function(locationName, finalCallback) {
-		nominatim.search({
-			q: locationName,
-		}, function(err, opts, results) {
-			console.log(results);
-			finalCallback();
-		});
-
-	}, function() {
-		console.log(features);
-	});
-});
+  var features = []
+  async.eachLimit(Object.keys(locations), 1, function (locationName, finalCallback) {
+    nominatim.search({
+      q: locationName
+    }, function (err, opts, results) {
+      if (err) {
+        console.error(err)
+      }
+      console.log(results)
+      finalCallback()
+    })
+  }, function () {
+    console.log(features)
+  })
+})
